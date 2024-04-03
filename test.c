1b1d6220e65aa6b76fe1cdb497b17a9c8ec8b709
Antoine-pa <antoine.pascal@gmx.fr>
2024-04-03 22:39:05 +0200

    editing tests

--- test.c
+++ test.c
@@ -11,17 +11,6 @@ void print_codes(char** codes) {
 }
 
 int main() {
-    /*f995-324746*/
-    tree_t t1 = init_tree(65, 5);
-    tree_t t2 = init_tree(66, 3);
-    tree_t t3 = merge_trees(t1, t2);
-    tree_t t4 = merge_trees(t3, init_tree(67, 9));
-    int h = tree_height(t4);
-    int n = tree_size(t4);
-    print_tree(t4);
-    printf("hauteur : %d, taille : %d\n", h, n);
-    free_tree(t4);
-
     char* text = "abcaccabcbbaefgc";
     heap_t heap = build_heap_from_text(text);
     tree_t tree = build_code_from_heap(heap);
